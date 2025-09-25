302bc7d1baabcb11c45ac2ff4ed7c410432fc820
connorsexton <cose6573@colorado.edu>
2025-09-24 18:04:41 -0600

    ica6

--- ica/ica6/ica6.js
+++ ica/ica6/ica6.js
@@ -13,9 +13,19 @@ filterButtons.forEach(button => {
 function filterPhotos(category) {
     photoCards.forEach(card => {
       if (category === 'all' || card.dataset.category === category) {
-        card.style.display = 'block';
+        card.style.display = 'all';
       } else {
-        card.style.display = 'none';
+        card.style.display = 'block';
+      }
+      if (category === 'nature' || card.dataset.category === category) {
+        card.style.display = 'nature';
+      } else {
+        card.style.display = 'block';
+      }
+      if (category === 'architecture' || card.dataset.category === category) {
+        card.style.display = 'architecture';
+      } else {
+        card.style.display = 'block';
       }
     });
   }
