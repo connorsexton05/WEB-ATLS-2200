00dfa1cb29dff67f1d401c043a722b87a98f12a5
connorsexton <cose6573@colorado.edu>
2025-10-21 10:40:24 -0600

    ica10

---
+++ ica/ica10/ica10.js
@@ -0,0 +1,44 @@
+let triviaBtn = document.querySelector("#js-new-quote").addEventListener('click', newTrivia);
+
+let answerBtn = document.querySelector('#js-tweet').addEventListener('click', newAnswer);
+
+const endpoint = "https://trivia.cyberwisp.com/getrandomchristmasquestion";
+ 
+let current = {
+    question: "",
+    answer: "",
+}
+async function newTrivia() {
+    // console.log("success!")
+    try {
+        const response = await fetch(endpoint);
+        if (!response.ok) {
+            throw Error(!response.statusText)
+        }    
+    const json = await response.json(); 
+    // console.log(json);
+    displayTrivia(json["question"]);
+    current.question = json["question"];
+    current.answer = json["answer"];
+    // console.log(current.question);
+} catch (err) {
+    console.log(err)
+    alert('Failed to get new trivia')
+    }
+}
+
+function displayTrivia(question) {
+    const questionText = document.querySelector('#js-quote-text');
+    questionText.textContent = question;
+    const answerText = document.querySelector('#js-answer-text');
+    questionText.textContent = question;
+    answerText.textContent = ""
+}
+
+function newAnswer() {
+    // console.log("Success == answer!")
+    const answerText = document.querySelector('#js-answer-text');
+    answerText.textContent = current.answer;
+}
+
+newTrivia();
